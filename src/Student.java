109ec6cf0f7e37df3c1ec8b6f9982288283fac5c
kaharnasyrhan <nwkahar08@mail.com>
2025-12-19 07:02:19 +0500

    added Student class

---
+++ src/Student.java
@@ -0,0 +1,42 @@
+public class Student {
+    private String name;
+    private int age;
+    private double gpa;
+
+    // Constructor
+    public Student(String name, int age, double gpa) {
+        this.name = name;
+        this.age = age;
+        this.gpa = gpa;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    // Method
+    public String getInfo() {
+        return "Student: " + name + ", Age: " + age + ", GPA: " + gpa;
+    }
+}
